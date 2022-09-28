f13d3a78c5893f74c4a6fe68e8571cd48c617770
SSagun-c <colinpeiffer6@gmail.com>
2022-09-28 18:22:23 +0200

    First time!

---
+++ serverleaver.py
@@ -0,0 +1,10 @@
+import time
+import pyautogui
+
+for i in range(200):
+    pyautogui.rightClick(-1882, 168)
+    time.sleep(1)
+    pyautogui.click(-1838, 474)
+    time.sleep(1)
+    pyautogui.click(-801, 582)
+    time.sleep(1)
---
+++ unfriender.py
@@ -0,0 +1,9 @@
+import pyautogui
+import time
+
+for i in range(161):
+    pyautogui.click(-473, 207)
+    pyautogui.click(-408, 291)
+    time.sleep(0.5)
+    pyautogui.click(-820, 575)
+    time.sleep(0.5)
